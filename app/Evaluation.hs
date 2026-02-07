2d6db0a5474222a0e9f9c94a294c02c1c0a8ecf7
Dylan Sommer <dylansommer96@gmail.com>
2026-02-07 16:16:02 -0500

    more haphazard changes

--- app/Evaluation.hs
+++ app/Evaluation.hs
@@ -1,19 +1,23 @@
 {-# LANGUAGE DeriveFunctor #-}
+
 module Evaluation where
 
-import Prelude hiding ((!))
-import qualified Data.Map as M
-import Data.Map ((!))
 import AST
+import Data.Map ((!))
+import qualified Data.Map as M
+import Prelude hiding ((!))
 
-data Ty = TInt | TBool | TChar | TStr | TVec deriving (Eq, Show)
+data Ty = TInt | TBool | TChar | TStr | TVec | TList deriving (Eq, Show)
 
 data Val
-  = VInt  Int
+  = VInt Int
   | VBool Bool
   | VChar Char
-  | VStr  String
-  | VVec  [Val]
+  | VStr String
+  | VCons Val Val
+  | VNull
+  | VVec [Val]
+  | VVoid
   deriving (Eq, Show)
 
 type Ctx = M.Map Id Val
@@ -21,9 +25,20 @@ type Ctx = M.Map Id Val
 evalOp0 :: Op0 -> IO Val
 evalOp0 ReadByte = VChar <$> getChar
 
--- | Throws an error if the identifier is not found; consider refactoring to a 
+-- | Throws an error if the identifier is not found; consider refactoring to a
 evalId :: Ctx -> Id -> Val
 evalId ctx id = ctx M.! id
 
-evalOp2 :: Ctx -> Op2 -> Expr -> Expr -> Val
-evalOp2 ctx op l r = undefined
+evalOp2 :: Op2 -> Val -> Val -> Val
+evalOp2 _ VVoid _ = error "attempted to operate on a void type"
+evalOp2 _ _ VVoid = error "attempted to operate on a void type"
+evalOp2 Plus (VInt l) (VInt r) = VInt (l + r)
+evalOp2 Minus (VInt l) (VInt r) = VInt (l - r)
+evalOp2 LessThan (VInt l) (VInt r) = VBool (l < r)
+evalOp2 Equals (VInt l) (VInt r) = VBool (l == r)
+evalOp2 Equals (VBool l) (VBool r) = VBool (l == r)
+evalOp2 Equals (VChar l) (VChar r) = VBool (l == r)
+evalOp2 Equals (VStr l) (VStr r) = VBool (l == r)
+evalOp2 Equals (VVec l) (VVec r) = VBool (l == r)
+evalOp2 Equals _ _ = VBool False
+evalOp2 Cons
