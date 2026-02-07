d5db42e2ab7ad4a5c07a2c59ae79e4bbb6e51d24
Edward Feng <ed@ewf.me>
2026-02-07 16:41:37 -0500

    add int to parser and re-export parseTest

--- app/Parser.hs
+++ app/Parser.hs
@@ -1,4 +1,4 @@
-module Parser where
+module Parser (module Parser, Text.Megaparsec.parseTest) where
 
 import Data.Void (Void)
 import Text.Megaparsec
@@ -10,3 +10,12 @@ type Parser = Parsec Void String
 
 parseBool :: Parser Expr
 parseBool = Bool False <$ string "#f" <|> Bool True <$ string "#t"
+
+parseIntPos :: Parser Expr
+parseIntPos = Int . read <$> some numberChar
+
+parseIntNeg :: Parser Expr
+parseIntNeg = Int . read <$> ((:) <$> char '-' <*> some numberChar)
+
+parseChar :: Parser Expr
+parseChar = Char <$> (char '\'' *> printChar <* char '\'')
