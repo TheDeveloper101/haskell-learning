f6022e6d4d8178fc4d96f59169d3ae4765032bc6
Edward Feng <ed@ewf.me>
2026-02-08 16:50:07 -0500

    glue parsing and compiling

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
 module Main where
 
+import System.Environment
+import System.IO
+
+import Codegen
+import CodegenWasm
+import Parser
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStr "> "
+  hFlush stdout
+  input <- getLine
+  args <- getArgs
+  case parseExpr input of
+    Left err -> putStrLn err
+    Right ast -> case head args of
+      "x86" -> compileAndLink ast
+      "wasm" -> runWasm ast >>= print
+      _ -> putStrLn "bad backend :("
