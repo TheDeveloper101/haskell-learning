604ba7a7f8a3e5f5bebe03f034be683305631f61
Dylan Sommer <dylansommer96@gmail.com>
2026-02-08 11:37:34 -0500

    idk

--- app/CodegenWasm.hs
+++ app/CodegenWasm.hs
@@ -8,14 +8,24 @@ module CodegenWasm (compileWasm) where
 
 import AST (Expr (..))
 import Language.Wasm.Structure (ValueType(..), Expression)
-import Language.Wasm.Builder 
+import Language.Wasm.Builder
+import Language.Wasm.Interpreter
 import Data.Proxy
 import Types(valueToBits, bitsToValue)
 
-compileWasm :: Expr -> GenFun (Proxy I64)
 
+runWasm :: Expr -> IO ()
+runWasm code = do let address = getAddress code
+                  invoke emptyStore (error "what address") []
+                  pure ()
+
+getAddress :: GenFun (Proxy I64) -> Int
+getAddress = error "get me the address pls"
+
+compileWasm :: Expr -> GenFun (Proxy I64)
 compileWasm e = case e of
     Int i -> i64c (valueToBits (Int i))
     Bool b -> i64c (valueToBits (Bool b))
     Char c -> i64c (valueToBits (Char c))
     _ -> trap Proxy
+
